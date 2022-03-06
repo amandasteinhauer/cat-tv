962ed8db539d046392317ba7c7dd5f78b1187de0
Amanda <amanda.steinhauer@bell.net>
2022-03-05 21:32:04 -0500

    add base template

--- catTV/README.md
+++ catTV/README.md
@@ -1,2 +1,6 @@
 # cat-tv
  
+To run:
+''' source venv/bin/activate '''
+''' export FLASK_APP=catTV '''
+''' flask run '''
--- catTV/routes.py
+++ catTV/routes.py
@@ -7,7 +7,7 @@ def home():
 
 @app.route("/about")
 def about():
-    return render_template('home.html')
+    return render_template('about.html')
 
 @app.route("/birds")
 def birds():
